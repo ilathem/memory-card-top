ca46e4218b055e0670695a6f365dbfb62a4caf5d
ilathem <isaiahlathem@gmail.com>
2025-01-28 10:44:09 -0600

    add back in the part accessing the api

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className='bg-black h-screen'>
       <h1 className='text-white text-6xl mb-5 text-center'>Memory!</h1>
-      {/* <CardContainer /> */}
+      <CardContainer />
     </div>
   );
 }
